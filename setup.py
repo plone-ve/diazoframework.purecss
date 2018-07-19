08f9f6ab03b56a733164e84dd201c259b6f30c7e
Leonardo J. Caballero G <leonardocaballero@gmail.com>
2018-07-19 06:00:22 -0400

    Changed the package documentation files

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ setup(name='diazoframework.purecss',
       version=version,
       description="A Diazo framework implementation for Pure CSS",
       long_description=open("README.rst").read() + "\n" +
-                       open(os.path.join("docs", "HISTORY.txt")).read(),
+                       open("CHANGES.txt").read(),
       # Get more strings from
       # https://pypi.org/pypi?:action=list_classifiers
       classifiers=[
